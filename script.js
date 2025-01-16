209581af44a705c9d79751fbc3c8b2f177325b07
Zim Do <zimd0110@gmail.com>
2025-01-16 22:31:06 +1100

    clean up and reorder script

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const BOARD_SIDE_LENGTH = 700;
 const DEFAULT_SQUARES_PER_SIDE = 16;
-
-const container = document.querySelector("#container");
-container.style.width = `${BOARD_SIDE_LENGTH}px`;
+const MAX_SQUARES_PER_SIDE = 100;
 
 constructBoard(DEFAULT_SQUARES_PER_SIDE);
 
@@ -10,29 +8,16 @@ const button = document.querySelector("button");
 const input = document.querySelector("input");
 button.addEventListener("click", handleButtonClick);
 
-
-function handleButtonClick(event) {
-    const squaresPerSide = parseInt(input.value, 10);
-    
-    if (Number.isInteger(squaresPerSide)) {
-        removeBoard();
-        constructBoard(squaresPerSide);
-        input.value = '';
-    } else {
-
-    }
-}
-
-function removeBoard() {
-    const grids = document.querySelectorAll("#container div");
-    grids.forEach((grid) => grid.remove());
-}
+///////////////
+// FUNCTIONS //
+///////////////
 
 function constructBoard(squaresPerSide) {
     for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
         let grid = document.createElement("div");
         grid.style.width = `${BOARD_SIDE_LENGTH/squaresPerSide}px`;
         grid.style.height = `${BOARD_SIDE_LENGTH/squaresPerSide}px`;
+        grid.style.borderWidth = `${16/squaresPerSide}px`;
         container.appendChild(grid);
     }
 
@@ -49,4 +34,19 @@ function makeBoardInteractive() {
     function handleHover(event) {
         event.target.style.backgroundColor = "black";
     }
+}
+
+function handleButtonClick() {
+    const squaresPerSide = parseInt(input.value, 10);
+    
+    if (Number.isInteger(squaresPerSide) && squaresPerSide <= MAX_SQUARES_PER_SIDE && squaresPerSide > 0) {
+        removeBoard();
+        constructBoard(squaresPerSide);
+        input.value = '';
+    }
+}
+
+function removeBoard() {
+    const grids = document.querySelectorAll("#container div");
+    grids.forEach((grid) => grid.remove());
 }
