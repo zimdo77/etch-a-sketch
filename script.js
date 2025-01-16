a92b1f1553e226a1d15b1cd4b87eb69e7f419ca6
Zim Do <zimd0110@gmail.com>
2025-01-16 21:59:08 +1100

    implement board resizing

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
-const container = document.querySelector("#container");
+const BOARD_SIDE_LENGTH = 700;
+const DEFAULT_SQUARES_PER_SIDE = 16;
 
-for (let i = 0; i < 16*16; i++) {
-    container.appendChild(document.createElement("div"));
+const container = document.querySelector("#container");
+container.style.width = `${BOARD_SIDE_LENGTH}px`;
+
+constructBoard(DEFAULT_SQUARES_PER_SIDE);
+
+const button = document.querySelector("button");
+const input = document.querySelector("input");
+button.addEventListener("click", handleButtonClick);
+
+
+function handleButtonClick(event) {
+    const squaresPerSide = parseInt(input.value, 10);
+    
+    if (Number.isInteger(squaresPerSide)) {
+        removeBoard();
+        constructBoard(squaresPerSide);
+        input.value = '';
+    } else {
+
+    }
 }
 
-const grids = document.querySelectorAll("#container div");
+function removeBoard() {
+    const grids = document.querySelectorAll("#container div");
+    grids.forEach((grid) => grid.remove());
+}
 
-grids.forEach((grid) => {
-    grid.addEventListener("mouseover", handleHover);
-})
+function constructBoard(squaresPerSide) {
+    for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+        let grid = document.createElement("div");
+        grid.style.width = `${BOARD_SIDE_LENGTH/squaresPerSide}px`;
+        grid.style.height = `${BOARD_SIDE_LENGTH/squaresPerSide}px`;
+        container.appendChild(grid);
+    }
 
-function handleHover(event) {
-    event.target.style.backgroundColor = "#D3D3D3";
+    makeBoardInteractive();
+}
+
+function makeBoardInteractive() {
+    const grids = document.querySelectorAll("#container div");
+
+    grids.forEach((grid) => {
+        grid.addEventListener("mouseover", handleHover);
+    });
+
+    function handleHover(event) {
+        event.target.style.backgroundColor = "black";
+    }
 }
